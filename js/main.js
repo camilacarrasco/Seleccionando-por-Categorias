0ee0715a4754f7cdb2e6111ead4fc3a3f427908b
camilacarrasco <cmarion.carrasco1@gmail.com>
2016-07-17 21:07:12 -0400

    cartelera lista para elegir categories

--- js/main.js
+++ js/main.js
@@ -1,9 +1,38 @@
-$(document).ready(function(){ 
-   $(".clasicas").on('click',function(event){ 
-      $(this).addClass("clases"); 
-      $(".container").addClass("BB"); 
-   }); 
-   $(".clasicas").mouseenter( function(){ 
-      $(".clasicas").removeClass("AA");       
-   }); 
-}); 
+$(document).ready(function(){
+	console.log("Hola Gracias por tu preferencia (Y)");
+});
+
+function mostrarAccion(){
+	$(".col-sm-3").show();
+	$(".col-sm-3").not(".Accion").hide();
+}
+
+function mostrarComedia(){
+	$(".col-sm-3").show();
+	$(".col-sm-3").not(".Comedia").hide();
+}
+
+function mostrarRomántica(){
+	$(".col-sm-3").show();
+	$(".col-sm-3").not(".Romántica").hide();
+}
+
+function mostrarInfantil(){
+	$(".col-sm-3").show();
+	$(".col-sm-3").not(".Infantil").hide();
+}
+
+function mostrarAventura(){
+	$(".col-sm-3").show();
+	$(".col-sm-3").not(".Aventura").hide();
+}
+
+function mostrarTerror(){
+	$(".col-sm-3").show();
+	$(".col-sm-3").not(".Terror").hide();
+}
+
+function mostrarTodas(){
+	$(".col-sm-3").show();
+}
+
